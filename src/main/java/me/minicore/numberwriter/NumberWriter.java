fc241b5bd534d5358ffac2e0e43444f6ba3144ef
adamsteele <adam@minicore.org>
2022-01-03 10:54:20 +0000

    Supports 3 digits

--- src/main/java/me/minicore/numberwriter/NumberWriter.java
+++ src/main/java/me/minicore/numberwriter/NumberWriter.java
@@ -19,14 +19,44 @@ public final class NumberWriter extends JavaPlugin {
             @Override
             public void run() {
                 int playerCount = Bukkit.getOnlinePlayers().size();
-                Boolean[][] blocks = numbers.getArray(playerCount);
+                int hundreds = playerCount / 100;
+                int tens = (playerCount - (hundreds * 100)) / 10;
+                int digit = (playerCount - (tens * 10));
+
+                Boolean[][] hundredsBlocks = numbers.getArray(hundreds);
+                Boolean[][] tensBlocks = numbers.getArray(tens);
+                Boolean[][] digitBlocks = numbers.getArray(digit);
 
                 for (int z = 2; z >= 0; z--) {
                     for (int y = 4; y >= 0; y--) {
                         Location location = new Location(Bukkit.getWorld("world"), -98, 25, -19);
                         location.add(0, (-y), (-z));
-                        if (blocks[y][z]) {
-                            Bukkit.getWorld("world").getBlockAt(location).setType(Material.GREEN_CONCRETE);
+                        if (digitBlocks[y][z]) {
+                            Bukkit.getWorld("world").getBlockAt(location).setType(Material.GREEN_TERRACOTTA);
+                        } else {
+                            Bukkit.getWorld("world").getBlockAt(location).setType(Material.BLACK_CONCRETE);
+                        }
+
+                    }
+                }
+                for (int z = 2; z >= 0; z--) {
+                    for (int y = 4; y >= 0; y--) {
+                        Location location = new Location(Bukkit.getWorld("world"), -98, 25, -15);
+                        location.add(0, (-y), (-z));
+                        if (tensBlocks[y][z]) {
+                            Bukkit.getWorld("world").getBlockAt(location).setType(Material.GREEN_TERRACOTTA);
+                        } else {
+                            Bukkit.getWorld("world").getBlockAt(location).setType(Material.BLACK_CONCRETE);
+                        }
+
+                    }
+                }
+                for (int z = 2; z >= 0; z--) {
+                    for (int y = 4; y >= 0; y--) {
+                        Location location = new Location(Bukkit.getWorld("world"), -98, 25, -11);
+                        location.add(0, (-y), (-z));
+                        if (hundredsBlocks[y][z]) {
+                            Bukkit.getWorld("world").getBlockAt(location).setType(Material.GREEN_TERRACOTTA);
                         } else {
                             Bukkit.getWorld("world").getBlockAt(location).setType(Material.BLACK_CONCRETE);
                         }
